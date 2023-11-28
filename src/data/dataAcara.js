98b2ddd03ada20c43d53d157ff079f98c71356d7
Reza Bagus Saputra <103319101+RezaBagusS@users.noreply.github.com>
2023-11-28 09:00:49 +0700

    adding profile page

--- src/data/dataAcara.js
+++ src/data/dataAcara.js
@@ -62,7 +62,54 @@ const dataUntukAndaAcara = [
 ]
 
 const dataRiwayatAcara = [
-
+    {
+        id: 1,
+        title: "Bazar Rakyat",
+        date: "10 September 2023",
+        img: "https://res.cloudinary.com/dr0lbokc5/image/upload/v1700878416/Group_202_5_lgkbwn.png",
+        tag: "Kuliner",
+        description: "Taklukkan lidah dengan kelezatan! Bergabunglah dengan kami di Bazar Rakyat Kuliner, tempat para koki lokal memamerkan keahlian kuliner terbaik mereka. Rasakan keanekaragaman cita rasa dan dukung UMKM lokal di acara kami.",
+        lokasi: "Malang, MCC Lt 3",
+        waktu: "Jumat, 13 Okt 2023",
+        jam: "15.00 - 21.00 WIB",
+        link: ""
+    },
+    {
+        id: 2,
+        title: "Bazar Amal",
+        date: "12 November 2023",
+        img: "https://res.cloudinary.com/dr0lbokc5/image/upload/v1700878473/Group_202_6_wcwsxr.png",
+        tag: "Lain-lain",
+        description: "Jadilah bagian dari perubahan positif! Bergabunglah dengan kami di Bazar Amal UMKM-Community, di mana setiap langkah kebaikanmu membantu mewujudkan impian para pelaku UMKM. Sambutlah kebaikan dengan tangan terbuka dan dukung perjuangan mereka untuk masa depan yang lebih cerah.",
+        lokasi: "J;. Guntur No.2, Malang",
+        waktu: "Sabtu, 16 Des 2023",
+        jam: "07.00 - 10.00 WIB",
+        link: ""
+    },
+    {
+        id: 3,
+        title: "Strategi Sukses Bisnis",
+        date: "15 September 2023",
+        img: "https://res.cloudinary.com/dr0lbokc5/image/upload/v1700878478/Group_202_7_jamxno.png",
+        tag: "Seminar",
+        description: "Jelajahi dunia Strategi Sukses Bisnis bersama kami! Saksikan para ahli berbagi wawasan mendalam tentang pengelolaan bisnis, pemasaran digital, dan inovasi yang dapat mendorong pertumbuhan UMKM. Mari bersama-sama membangun masa depan yang berkelanjutan untuk bisnis lokal!",
+        lokasi: "Zoom Meeting",
+        waktu: "Kamis, 30 Nov 2023",
+        jam: "09.00 - 12.00 WIB",
+        link: ""
+    },
+    {
+        id: 4,
+        title: "Strategi Pemasaran Digital",
+        date: "10 November 2023",
+        img: "https://res.cloudinary.com/dr0lbokc5/image/upload/v1700878481/Group_202_8_losatj.png",
+        tag: "Seminar",
+        description: "Selami rahasia kesuksesan di era digital! Bergabunglah dalam Seminar Strategi Pemasaran Digital kami dan temukan cara efektif memanfaatkan teknologi untuk mengangkat bisnismu ke level berikutnya. Mari bersama-sama membentuk visi baru bagi UMKM lokal!",
+        lokasi: "Malang, MCC Lt 3",
+        waktu: "Jumat, 13 Okt 2023",
+        jam: "15.00 - 21.00 WIB",
+        link: ""
+    },
 ]
 
 export {dataUntukAndaAcara, dataRiwayatAcara};
